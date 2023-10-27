970f86ac2446017bf527efe5751e57d8a6921953
Giorgia Bosello <giorgia.bosello@filoblu.com>
2023-10-27 09:14:43 +0200

    :bug: fix: onChange callback not triggered #140

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,5 @@
 export const isArraysEqualEps = (arrayA: number[], arrayB: number[], eps = 0.000001): boolean => {
-	if (arrayA && arrayB) {
+	if (arrayA.length && arrayB.length) {
 		for (let i = 0; i !== arrayA.length; ++i) {
 			if (Math.abs(arrayA[i] - arrayB[i]) > eps) {
 				return false
