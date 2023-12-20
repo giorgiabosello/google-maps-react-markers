4e925c6dfd8bcc88079112362ae99463ac0cf492
Giorgia Bosello <giorgia.bosello@filoblu.com>
2023-12-20 23:08:45 +0100

    :green_heart: fix CI build

--- docs/src/components/marker.tsx
+++ docs/src/components/marker.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 interface MarkerProps {
 	className?: string
-	draggable?: boolean
+	draggable: boolean
 	lat: number
 	lng: number
 	markerId: string
@@ -16,13 +16,27 @@ interface MarkerProps {
 	onDragStart?: (e: React.MouseEvent<HTMLImageElement, MouseEvent>, props: { latLng: LatLngLiteral }) => void
 }
 
-const Marker = ({ className, lat, lng, markerId, onClick, ...props }: MarkerProps) =>
+const Marker = ({
+	className,
+	lat,
+	lng,
+	markerId,
+	onClick,
+	draggable,
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	onDrag,
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	onDragEnd,
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	onDragStart,
+	...props
+}: MarkerProps) =>
 	lat && lng ? (
 		// eslint-disable-next-line @next/next/no-img-element, jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
 		<img
 			className={className}
 			src={`${process.env.NEXT_PUBLIC_MEDIA_URL ?? '/google-maps-react-markers'}/marker-pin${
-				props?.draggable ? '-draggable' : ''
+				draggable ? '-draggable' : ''
 			}.png`}
 			// lat={lat}
 			// lng={lng}
